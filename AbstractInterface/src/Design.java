86f8c535a1d26218b1154842e3f0f48033cbcde6
wpj0110 <84047863+wpj0110@users.noreply.github.com>
2022-05-25 18:46:18 -0500

    Notes fix

--- AbstractInterface/src/Design.java
+++ AbstractInterface/src/Design.java
@@ -2,11 +2,19 @@ public interface Design {
     void FillColor();
     int add(int a, int b);
 
-    //int giveNum(){ //Everything within an interface is abstract therefore it can't have a body... unless it's static
+    //int giveNum(){ //Everything within an interface is abstract therefore it can't have a body... unless it's static, default, or private
     //    return 1;
     //}
 
     static int giveNum(){ //this is possible
+        return 1;
+    }
+
+    default int giveNum2(){ //this is possible
+        return 1;
+    }
+
+    private int giveNum3(){ //this is possible
         return 1;
     }
 }
