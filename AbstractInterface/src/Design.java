0d884ad19c709e51967a57ce37332697f0f5ae28
wpj0110 <84047863+wpj0110@users.noreply.github.com>
2022-05-25 18:44:48 -0500

    Notes Correction

--- AbstractInterface/src/Design.java
+++ AbstractInterface/src/Design.java
@@ -2,7 +2,11 @@ public interface Design {
     void FillColor();
     int add(int a, int b);
 
-    //int giveNum(){ //Everything within an interface is abstract therefore it can't have a body.
+    //int giveNum(){ //Everything within an interface is abstract therefore it can't have a body... unless it's static
     //    return 1;
     //}
+
+    static int giveNum(){ //this is possible
+        return 1;
+    }
 }
