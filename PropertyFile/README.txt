f36f26f46206b7baa97a3014a2fbddbd626d44af
wpj0110 <84047863+wpj0110@users.noreply.github.com>
2022-06-07 15:45:12 -0500

    PropertyFile added

---
+++ PropertyFile/README.txt
@@ -0,0 +1,4 @@
+Reference: https://www.w3resource.com/java-tutorial/java-propertyfile-processing.php#:~:text=Summary%3A%201%20Properties%20file%20provide%20flexibility%20in%20terms,Read%2FWrite%20property%20file%20using%20java.util.Properties%20library%20of%20Java.
+
+Put the property file in the root folder.
+Useful link: https://www.codingeek.com/java/read-and-write-properties-file-in-java-examples/#:~:text=FileOutputStream%20%28%E2%80%9Cconfig.properties%E2%80%9D%29%20is%20used%20so%20as%20to%20either,if%20you%20want%20to%20store%20it%20somewhere%20else.
---
+++ PropertyFile/src/propertiesfile/PropertyFileReading.java
@@ -0,0 +1,46 @@
+package propertiesfile;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileReading {
+    public static void main(String[] args) {
+
+        //loading the property file
+        Properties prop = new Properties();
+        try {
+            // load a properties file for reading
+            prop.load(new FileInputStream("myConfig.properties"));
+            // get the properties and print
+            prop.list(System.out);
+            //Reading each property value
+            System.out.println(prop.getProperty("FileName"));
+            System.out.println(prop.getProperty("Author_Name"));
+            System.out.println(prop.getProperty("Website"));
+            System.out.println(prop.getProperty("TOPIC"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //writing to a property file
+        //a new file is created if the file name doesn't exist. Otherwise, it just replaces the old property file.
+        Properties prop2 = new Properties();
+        try {
+            // set the properties value
+            prop2.setProperty("Gujarat", "Gandhinagar");
+            prop2.setProperty("Maharashtra", "Mumbai");
+            prop2.setProperty("Madhya_Pradesh", "Indore");
+            prop2.setProperty("Uttar_Pradesh", "Lucknow");
+            // save properties to project root folder
+            prop2.store(new FileOutputStream("capitals.properties"), null);
+
+            prop2.setProperty("Illinois", "Springfield"); //adds Illinois
+            prop2.setProperty("Texas", "Dallas"); //adds Texas
+
+            prop2.store(new FileOutputStream("capitals.properties"), null);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
